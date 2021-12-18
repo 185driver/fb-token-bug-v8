cad5de4b2fdf4de8143fdb0a79a1519bdf84f052
Doug Allrich <dougallrich@gmail.com>
2021-12-18 08:12:41 -0600

    chore: rename project

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:185driver/fb-token-bug.git master:gh-pages
+git push -f git@github.com:185driver/fb-token-bug-v8.git master:gh-pages
 
 cd -
