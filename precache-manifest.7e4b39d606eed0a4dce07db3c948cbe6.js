cf8d12c991ad60c0fa476dcc0d378fc4780ca51b
Doug Allrich <dougallrich@gmail.com>
2021-12-19 06:52:07 -0600

    'deploy'

---
+++ precache-manifest.7e4b39d606eed0a4dce07db3c948cbe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dccd9b3ee4b0d892150",
+    "url": "/fb-token-bug-v8/css/app.4eefdf34.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/fb-token-bug-v8/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fbdfd266078d52c6d52197e632c63950",
+    "url": "/fb-token-bug-v8/index.html"
+  },
+  {
+    "revision": "4dccd9b3ee4b0d892150",
+    "url": "/fb-token-bug-v8/js/app.2e070a81.js"
+  },
+  {
+    "revision": "269db55adc13cefab440",
+    "url": "/fb-token-bug-v8/js/chunk-vendors.677ffa73.js"
+  },
+  {
+    "revision": "3903c43991f15536d79d3b56b0245195",
+    "url": "/fb-token-bug-v8/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fb-token-bug-v8/robots.txt"
+  }
+]);
